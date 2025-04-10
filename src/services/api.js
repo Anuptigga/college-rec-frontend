1cf2c9ed3b215a62384ebb3f0e380bd3f5f2cc86
Anup <anuptigga21@gmail.com>
2025-04-10 10:47:11 +0530

    fix:api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_SERVER
-  ,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
